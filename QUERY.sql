94451667693a050caf116241dcd346178f667143
Hasan Rafi Ahmed <hasan.cse570@gmail.com>
2026-06-14 15:06:49 +0600

    4th  question ans query add

--- QUERY.sql
+++ QUERY.sql
@@ -68,3 +68,10 @@ WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%';
 -- 3rd question ans
 SELECT booking_id, user_id, match_id, COALESCE(payment_status, 'Action Required') AS systematic_status 
 FROM Bookings WHERE payment_status IS NULL;
+
+
+-- 4th question ans
+SELECT b.booking_id, u.full_name, m.fixture, b.total_cost
+FROM Bookings b
+INNER JOIN Users u ON b.user_id = u.user_id
+INNER JOIN Matches m ON b.match_id = m.match_id;
